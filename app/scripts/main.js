a1095e6dfa5069c0700d78bb4b75e172e51a76cf
heldrida <heldrida@gmail.com>
2014-07-27 01:23:24 +0000

    generated using yeoman backbone generator. prepared sass with bourbon.

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global AdrianadoiCom, $*/
+
+
+window.AdrianadoiCom = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    AdrianadoiCom.init();
+});
