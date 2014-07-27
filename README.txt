e4aa2ef81e08d922a5ad88ffd6924950783740a8
heldrida <heldrida@gmail.com>
2014-07-27 04:04:39 +0000

    created rough version route and home view.

--- README.txt
+++ README.txt
@@ -8,3 +8,5 @@
 	Reference:
 
 		http://www.misakikawai.com/
+		http://code.tutsplus.com/tutorials/building-apps-with-the-yeoman-workflow--net-33254
+		http://coenraets.org/directory/#
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,118 @@
 /*global AdrianadoiCom, $*/
 
+'use strict';
 
 window.AdrianadoiCom = {
+
     Models: {},
+
     Collections: {},
-    Views: {},
-    Routers: {},
+
+    Views: {
+
+        MenuView: Backbone.View.extend({
+
+            el: '#menu',
+
+            events: {
+
+                'click li': 'open'
+
+            },
+
+            initialize: function(){
+
+                console.log('foo');
+
+                this.render();
+
+            },
+
+            open: function( event ){
+
+                console.log('open: ' + $(event.currentTarget).text() );
+
+            },
+
+            render: function(){
+                
+                return this;
+
+            }
+
+        }),
+
+        HomeView: Backbone.View.extend({
+
+            render: function(){
+
+                var template = _.template( $('#homeView').html(), {} );
+
+                console.log( template );
+
+                this.$el.html( template );
+
+                return this;
+
+            },
+
+            initialize: function(){
+
+                this.render();
+
+            }
+
+        })
+
+    },
+
+    Routers: {
+
+        Router: Backbone.Router.extend({
+
+            routes: {
+
+                'home': 'home'
+
+            },
+
+            defaultRoute: function() {
+
+                //this.home();
+
+            },
+
+            initialize: function(){
+
+                this.$content = $('#content');
+
+            },
+
+            home: function(){
+
+                var template = new AdrianadoiCom.Views.HomeView();
+
+                this.$content.html( template.el );
+
+            }
+
+        })
+
+    },
+
     init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
+
+        new AdrianadoiCom.Views.MenuView();
+        new AdrianadoiCom.Routers.Router();
+
+        Backbone.history.start();
+
     }
+
 };
 
 $(document).ready(function () {
-    'use strict';
+
     AdrianadoiCom.init();
+
 });
